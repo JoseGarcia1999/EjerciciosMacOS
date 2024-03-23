3c5c588e44b1211f59ddfe8e7211d1b1d29012d6
orlando <orlando.barrera@mail.utec.edu.sv>
2024-03-23 13:42:54 -0600

    Initial Commit

---
+++ clnYoutube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clnYoutube
+//
+//  Created by Macbook on 3/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clnYoutube/clnYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  clnYoutubeApp.swift
+//  clnYoutube
+//
+//  Created by Macbook on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct clnYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
